a3d1054af49005102721fee3e4a7dd65e943f53e
manueledwardopaez <manueledwardopaez@gmail.com>
2023-11-08 21:09:55 -0400

    change images routes

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -14,7 +14,7 @@ export const HeroCard = ({
   first_appearance,
   characters,
 }) => {
-  const heroImageUrl = `../../../assets/heroes/${id}.jpg`;
+  const heroImageUrl = `./assets/heroes/${id}.jpg`;
   return (
     <div className="col animate__animated animate__fadeIn">
       <div className="card">
